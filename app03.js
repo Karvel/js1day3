1e31ec86f61ecb9019bb0ae70755d1577d23febe
karvel <jadeilyn@yahoo.com>
2015-11-09 23:41:30 -0800

    Initial commit. More intro functions and intro control flow in JavaScript.

---
+++ app03.js
@@ -0,0 +1,17 @@
+/*
+Elanna Grossman
+November 9, 2015
+GeekWise Javascript I
+*/
+
+var userChoice = prompt("What would you like to do?");
+
+if(userChoice === "")
+{
+  alert("You must answer; try again. Program terminated.");
+}//end if
+else
+{
+  alert(userChoice);
+  console.log(userChoice);
+}//end else
